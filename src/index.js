e64b16b1910fd708329002b88bac5ba73d2d23d3
David Arevalo <davidarevaloc20@gmail.com>
2025-05-15 11:12:57 -0600

    update: modify zoom meeting details and disable fake camera switching

--- src/index.js
+++ src/index.js
@@ -221,7 +221,7 @@ class ZoomAutomation {
       await this.navigateToZoom();
       await this.setupZoomFrame();
       await this.muteMicrophone();
-      await this.switchToFakeCam();
+      // await this.switchToFakeCam();
       await this.joinMeeting();
       logStep("Successfully joined Zoom meeting!");
       await this.sendAdminMessage();
